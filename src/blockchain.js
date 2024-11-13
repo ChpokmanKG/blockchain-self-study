a28e6e6ce0be008b4ae9c8b45029f46b41336604
Chyngyz Usenov <cusenov785@gmail.com>
2024-11-13 22:44:57 +0600

    1 Implemented transaction and fees

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,4 +1,41 @@
-const { generateHash } = require('./utils');
+const EC = require('elliptic').ec;
+const { generateHash, keys} = require('./utils');
+
+const ec = new EC('secp256k1');
+
+class Transaction {
+  constructor(from, to, amount, fee) {
+    this.from = from;
+    this.to = to;
+    this.amount = amount;
+    this.timestamp = Date.now();
+    this.fee = fee;
+  }
+
+  generateHash() {
+    return generateHash(this.timestamp + this.from + this.to + this.amount);
+  }
+
+  sign(key) {
+    if (key.getPublic('hex') !== this.from) {
+      throw new Error('You cannot affect for other wallet');
+
+      return;
+    }
+
+    const hashTx = this.generateHash();
+    const sig = key.sign(hashTx, 'base64');
+
+    this.signature = sig.toDER('hex');
+  }
+
+  isValid() {
+    if (this.from === null) return true;
+    if (!this.signature) return false;
+
+    return ec.keyFromPublic(this.from, 'hex').verify(this.generateHash(), this.signature);
+  }
+}
 
 class Block {
   constructor(timestamp, data = [], prevHash = '') {
@@ -30,19 +67,76 @@ class Block {
 class Blockchain {
   constructor() {
     this.chain = [new Block(Date.now(), [])];
-    this.difficulty = 5;
+    this.pendingTransactions = [];
+    this.difficulty = 3;
+    this.reward = 100;
   }
 
   getLastBlock() {
     return this.chain[this.chain.length - 1];
   }
 
-  addNewBlock(transactions) {
-    const newBlock = new Block(Date.now(), transactions, this.getLastBlock().hash);
+  minePendingTransactions(address) {
+    const rewardTx = new Transaction(
+      null,
+      address,
+      this.reward
+    );
+    let totalFees = 0;
 
-    newBlock.mine(this.difficulty);
+    for (const tx of this.pendingTransactions) {
+      totalFees += tx.fee;
+    }
 
-    this.chain.push(newBlock);
+    rewardTx.amount += totalFees;
+    this.pendingTransactions.push(rewardTx);
+
+    const block = new Block(
+      Date.now(),
+      this.pendingTransactions,
+      this.getLastBlock().hash
+    );
+
+    block.mine(this.difficulty);
+
+    console.log('Block successfully mined!');
+    this.chain.push(block);
+
+    this.pendingTransactions = [];
+  }
+
+  addTransaction(tx) {
+    if (!tx.from || !tx.to) throw new Error('Transaction must include from and to address');
+    if (!tx.isValid()) throw new Error('Cannot add invalid transaction to chain');
+    if (tx.amount <= 0) throw new Error('Transaction amount should be higher than 0');
+    if (tx.fee > tx.amount) throw new Error('Fee cannot be greater than transaction amount');
+
+    const addressBalance = this.getBalanceOfAddress(tx.from);
+
+    if (tx.amount > addressBalance) throw new Error('Not enough tokens');
+
+    const totalPendingAmount = this.pendingTransactions
+      .filter(pendingTx => tx.from === pendingTx.from)
+      .reduce((prev, curr) => prev + curr.amount, tx.amount);
+
+    if (totalPendingAmount > addressBalance) {
+      throw new Error('Not enough tokens');
+    }
+
+    this.pendingTransactions.push(tx);
+  }
+
+  getBalanceOfAddress(address) {
+    let balance = 0;
+
+    this.chain.forEach(block => {
+      block.data.forEach(tx => {
+        if (tx.from === address) balance = (balance - tx.amount) - tx.fee;
+        if (tx.to === address) balance = balance + tx.amount;
+      });
+    });
+
+    return balance;
   }
 
   checkIsValid() {
@@ -53,19 +147,33 @@ class Blockchain {
       if (currBlock.hash !== currBlock.generateHash() || prevBlock?.hash !== currBlock.prevHash) {
         return false
       }
+
+      for (const tx of currBlock.data) {
+        if (!tx.isValid()) return false;
+      }
     }
 
     return true;
   }
 }
 
+const minersKey = ec.keyFromPrivate(keys.minersPrivate);
+const minersWallet = minersKey.getPublic('hex');
+
+const key = ec.keyFromPrivate(keys.private);
+const wallet = key.getPublic('hex');
+
 const ChyngyzCoin = new Blockchain();
 
-ChyngyzCoin.addNewBlock([{txt: 'Hello'}, {txt: 'World'}]);
-ChyngyzCoin.addNewBlock([{txt: 'Hello'}, {txt: 'World'}]);
-ChyngyzCoin.addNewBlock([{txt: 'Hello'}, {txt: 'World'}]);
-ChyngyzCoin.addNewBlock([{txt: 'Hello'}, {txt: 'World'}]);
-ChyngyzCoin.addNewBlock([{txt: 'Hello'}, {txt: 'World'}]);
+ChyngyzCoin.minePendingTransactions(wallet);
 
-console.log(ChyngyzCoin.chain);
-console.log(ChyngyzCoin.checkIsValid());
+const tx1 = new Transaction(wallet, 'some_address', 50, 5);
+tx1.sign(key);
+ChyngyzCoin.addTransaction(tx1);
+
+ChyngyzCoin.minePendingTransactions(minersWallet);
+
+console.log('My wallet',ChyngyzCoin.getBalanceOfAddress(wallet));
+console.log('some_address',ChyngyzCoin.getBalanceOfAddress('some_address'));
+console.log('miners wallet',ChyngyzCoin.getBalanceOfAddress(minersWallet));
+console.log('blockchain validity',ChyngyzCoin.checkIsValid());
--- src/utils.js
+++ src/utils.js
@@ -4,4 +4,10 @@ const generateHash = (str) => {
   return crypto.createHash('sha256').update(str).digest('hex');
 }
 
-module.exports = {generateHash};
+// Do not repeat at home!
+const keys = {
+  private: '324af0d6b4a835370d37c3f2aab409259a52624dfe52b2ba9f2d4d92a818ec7c',
+  minersPrivate: 'b617594ee01f19cd80832c85c34613293a00444e6dd0352d1dc5b3d3f3c20e80'
+};
+
+module.exports = {generateHash, keys};
